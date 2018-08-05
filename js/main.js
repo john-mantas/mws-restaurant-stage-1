b21cf3a9ca78f8dac4a1253163b7dc3242e9d1dd
John Mantas <mantasjohn67@gmail.com>
2018-08-05 19:32:50 +0300

    fix: Make restaurant listings responsive

--- js/main.js
+++ js/main.js
@@ -162,23 +162,26 @@ createRestaurantHTML = (restaurant) => {
     image.className = 'restaurant-img';
     image.src = DBHelper.imageUrlForRestaurant(restaurant);
     li.append(image);
+    
+    const restaurantInfo = document.createElement('div');
+    li.append(restaurantInfo);
 
     const name = document.createElement('h1');
     name.innerHTML = restaurant.name;
-    li.append(name);
+    restaurantInfo.append(name);
 
     const neighborhood = document.createElement('p');
     neighborhood.innerHTML = restaurant.neighborhood;
-    li.append(neighborhood);
+    restaurantInfo.append(neighborhood);
 
     const address = document.createElement('p');
     address.innerHTML = restaurant.address;
-    li.append(address);
+    restaurantInfo.append(address);
 
     const more = document.createElement('a');
     more.innerHTML = 'View Details';
     more.href = DBHelper.urlForRestaurant(restaurant);
-    li.append(more)
+    restaurantInfo.append(more)
 
     return li
 }
