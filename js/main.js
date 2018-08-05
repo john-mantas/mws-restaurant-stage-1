065b74211d1578c5bdcb481ddab6ae2535ac2d4c
John Mantas <mantasjohn67@gmail.com>
2018-08-05 23:45:58 +0300

    feat: Add accessibility features

--- js/main.js
+++ js/main.js
@@ -182,6 +182,7 @@ createRestaurantHTML = (restaurant) => {
     const more = document.createElement('a');
     more.innerHTML = 'View Details';
     more.href = DBHelper.urlForRestaurant(restaurant);
+    more.setAttribute('aria-label', `View details for the "${restaurant.name}" restaurant`);
     restaurantInfo.append(more)
 
     return li
